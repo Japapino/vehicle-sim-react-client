44d917148f4da804948cd486883541256218271a
Stefan M <stefan.murakami@gmail.com>
2025-01-28 23:39:17 -0600

    formatting

--- src/LoanCalculator.tsx
+++ src/LoanCalculator.tsx
@@ -5,43 +5,58 @@ function LoanCalculator() {
   const [rate, setRate] = useState<number>(0);
   const [time, setTime] = useState<number>(0);
   const [emi, setEmi] = useState<number>(0);
-  const [formComplete, setFormComplete] = useState<boolean>(false); 
+  const [formComplete, setFormComplete] = useState<boolean>(false);
 
   const calculateEMI = () => {
     const monthlyRate = rate / 12 / 100;
-    const emi = (principal * monthlyRate * Math.pow(1 + monthlyRate, time)) / (Math.pow(1 + monthlyRate, time) - 1);
+    const emi =
+      (principal * monthlyRate * Math.pow(1 + monthlyRate, time)) /
+      (Math.pow(1 + monthlyRate, time) - 1);
     setEmi(Number(emi.toFixed(2)));
   };
 
   useEffect(() => {
-    console.log(principal, rate, time);
-    if (principal > 0 && rate > 0 && time > 0) setFormComplete(true); 
-  }, [principal, rate, time]); 
+    if (principal > 0 && rate > 0 && time > 0) setFormComplete(true);
+  }, [principal, rate, time]);
 
-  console.log(formComplete); 
+  console.log(formComplete);
 
+  // TODO: styling
+  // TODO: add real time updating
   return (
     <div className="loan-calculator">
       <h2>Loan Calculator</h2>
       <div className="form-field animation a3">
-        <label>
-          Principal Amount:
-          <input type="number" value={principal} onChange={(e) => setPrincipal(Number(e.target.value))} />
-        </label>
+        <div className="input-container">
+          <p>Principal Amount:</p>
+          <input
+            type="number"
+            value={principal}
+            onChange={(e) => setPrincipal(Number(e.target.value))}
+          />
+        </div>
       </div>
       <div className="form-field animation a2">
-        <label>
-          Annual Interest Rate (%):
-          <input type="number" value={rate} onChange={(e) => setRate(Number(e.target.value))} />
-        </label>
+        <div className="input-container">
+          <p>Annual Interest Rate (%):</p>
+          <input
+            type="number"
+            value={rate}
+            onChange={(e) => setRate(Number(e.target.value))}
+          />
+        </div>
       </div>
       <div className="form-field animation a3">
-        <label>
-          Loan Tenure (months):
-          <input type="number" value={time} onChange={(e) => setTime(Number(e.target.value))} />
-        </label>
+        <div className="input-container">
+          <p>Loan Tenure (months):</p>
+          <input
+            type="number"
+            value={time}
+            onChange={(e) => setTime(Number(e.target.value))}
+          />
+        </div>
       </div>
-      
+
       {formComplete && <button onClick={calculateEMI}>Calculate EMI</button>}
       {emi > 0 && (
         <div>

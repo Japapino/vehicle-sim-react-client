c9011dd03d3928eed79eb66bf660b4a5e97be70c
Stefan M <stefan.murakami@gmail.com>
2025-01-28 20:28:09 -0600

    add react router and tsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-/* eslint-disable react/no-unknown-property */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import "./App.scss";
+import LoanCalculator from "./CalculatorSim.tsx";
 import Modal from "./Modal.tsx";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
       <div className="container">
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/sim">Loan Calculator</Link>
+        </nav>
         <div className="left">
           <div className="header">
-            <h2 className="animation a1">Welcome to the Vehicle Price Clculator</h2>
+            <h2 className="animation a1">
+              Welcome to the Vehicle Price Clculator
+            </h2>
             <h4 className="animation a2">
-              Let&apos;s start by entering the year of your vehicle. 
+              Let&apos;s start by entering the year of your vehicle.
             </h4>
           </div>
-          <Modal />
+          <Routes>
+            <Route path="/" element={<Modal />} />
+            <Route path="/sim" element={<LoanCalculator />} />
+          </Routes>
         </div>
         <div className="right"></div>
       </div>
+    </Router>
   );
 }
 
